40be12297af1e377aa7e49085d2f36e1b20d4758
Verdo Daviarta <benedictusverdoartha@gmail.com>
2024-02-21 21:42:52 +0700

    Initial commit

---
+++ src/test/java/HomeScreenTest.java
@@ -0,0 +1,58 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import jdk.jshell.execution.Util;
+import org.example.Utils;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+    public class HomeScreenTest {
+
+        AndroidDriver driver;
+
+        @BeforeClass
+        public void setUp() throws MalformedURLException {
+            DesiredCapabilities dc = new DesiredCapabilities();
+            dc.setCapability("deviceName","Pixel 2 API 30");
+            dc.setCapability("udid","emulator-5554");
+            dc.setCapability("platformName","android");
+            dc.setCapability("platformVersion","11");
+            dc.setCapability("appPackage","id.tix.android");
+            dc.setCapability("appActivity","id.tix.android.splash.view.SplashActivity");
+            dc.setCapability("noReset",true);
+            URL url = new URL("http://127.0.0.1:4723/wd/hub");
+            driver = new AndroidDriver(url, dc);
+        }
+
+        @AfterClass
+        public void finish(){
+            Utils.delay(3);
+            driver.quit();
+        }
+
+        @Test
+        public void testNameCity() {
+            Utils.delay(20);
+            MobileElement city = (MobileElement) driver.findElementById("id.tix.android:id/tv_city");
+            city.click();
+            Utils.delay(5);
+            MobileElement formSearch = (MobileElement) driver.findElementById("id.tix.android:id/edt_search");
+            formSearch.sendKeys("Bandung");
+            Utils.delay(5);
+            MobileElement listCityName = (MobileElement) driver.findElementById("id.tix.android:id/tvCityName");
+            listCityName.click();
+            //Assert validasi
+            Utils.delay(5);
+            String strCityName = city.getText();
+            System.out.println(strCityName);
+            Assert.assertEquals(strCityName,"AGAK LAEN");
+
+        }
+
+
+    }
